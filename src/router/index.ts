bad6b69b746ef2d3885452a38cd80e0635c654d9
Brent Buchholz <brentbuchholz@gmail.com>
2023-06-08 10:23:19 -0500

    Add venture submission link

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import VentureView from "../views/VentureView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/venture",
+    name: "venture",
+    component: VentureView,
   },
   {
     path: "/about",
---
+++ src/views/VentureView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="venture">
+    <h1>Are you ready?</h1>
+    <p>
+      When you embark on a new learning path, you open your self and your circle
+      up to to avenues of exploration and discovery.
+    </p>
+    <p>
+      We encourage goal setting and assessment before, after, and during the
+      process, and so when submitting a link we ask that you include a video or
+      textual submission discussing the following items
+    </p>
+    <p>
+      What are you hoping to build with this tutorial? We value deliverables at
+      SCA, and encourage every project to be centered around something that
+      serves the greater circle and it's growth. If we fall short in our
+      venture, this also helps us redirect course in a way that preserves any
+      gains made in the process
+    </p>
+    <p>
+      <a
+        href="mailto:syndicated.circuitry@gmail.com?subject=SCA%20-%20Venture%20Information%20Submission&body=Link%20To%20Venture%20(submit%20tutorial%20link)%20-%3E%0A%0ALink%20To%20Goals%20Reflection%20(doc%20or%20vid%20is%20fine)%20-%3E"
+        target="_blank"
+        >Submit A Venture</a
+      >
+    </p>
+  </div>
+</template>
