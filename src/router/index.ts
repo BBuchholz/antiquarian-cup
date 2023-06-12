f40a05333df07cfe7aba7900f21adc336be88afc
Brent Buchholz <brentbuchholz@gmail.com>
2023-06-11 23:55:02 -0500

    Add learning path from submission

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import VentureView from "../views/VentureView.vue";
+import PathsView from "../views/PathsView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/venture",
     name: "venture",
     component: VentureView,
+  },
+  {
+    path: "/paths",
+    name: "paths",
+    component: PathsView,
   },
   {
     path: "/about",
---
+++ src/views/PathsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="paths">
+    <h1>This is the paths page</h1>
+    <p>
+      Learning Path: Intention To Explore Android Options (with a common
+      codebase)
+    </p>
+    <p>
+      Intentional Establishment Document:
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vTs9qkRR5QxM5OyOwa3MVLufzUaYNHRYThiTnXIKX1VbV4GdugtDSCZKbLQtHIK3-r1RqCbMAXauflC/pub"
+        target="_blank"
+      >
+        https://docs.google.com/document/d/e/2PACX-1vTs9qkRR5QxM5OyOwa3MVLufzUaYNHRYThiTnXIKX1VbV4GdugtDSCZKbLQtHIK3-r1RqCbMAXauflC/pub</a
+      >
+    </p>
+    <p>
+      Submitted By: BMB (intention submitted, awaiting first progress report)
+    </p>
+    <p>
+      Tutorial Link:
+      <a href="https://link.medium.com/7KeyOW3PsAb" target="_blank">
+        https://link.medium.com/7KeyOW3PsAb</a
+      >
+    </p>
+  </div>
+</template>
